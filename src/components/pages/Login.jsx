9c27224d58fa83b3e0237c6da47f7729dc588e55
RadArde123 <radu.ardelean15.92@gmail.com>
2025-01-24 01:56:36 +0200

    3th commit

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,5 @@
 import img3 from "../../assets/japan.jpg";
 
-import { FaGoogle } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
 import React,{useState} from 'react';
 import { CiUser, CiLock } from "react-icons/ci";
 
@@ -23,14 +21,14 @@ function Login(){
               <>
                 <div class=" w-full lg:h-[90%] lg:w-1/2 h-full border-none "> 
                 <form class="flex flex-col justify-center items-center w-full h-full"> 
-                  <h1 class="text-4xl font-bold text-white mt-32">Sign up</h1> 
+                  <h1 class="text-4xl  text-white mt-32 font-extrabold">Sign up</h1> 
                   <div class="flex flex-col justify-center items-center w-full p-10 mt-5 h-full gap-3 ">
                       <input
                         type="text"
                         name="firstName"
                         required
                         placeholder="First Name"
-                        className="w-full border-2 border-gray-400 rounded-md lg:p-2"
+                        className="w-full text-white font-extrabold border-2 border-gray-400 rounded-md lg:p-2 bg-inherit outline-none"
                       ></input>
      
                       <input
@@ -38,7 +36,7 @@ function Login(){
                         name="lastName"
                         required
                         placeholder="Last Name"
-                        className="w-full border-2 border-gray-400 rounded-md lg:p-2"
+                        className="w-full  text-white font-extrabold  border-2 border-gray-400 rounded-md lg:p-2 bg-inherit outline-none"
                       ></input>
     
                       <input
@@ -46,7 +44,7 @@ function Login(){
                         name="email"
                         required
                         placeholder="Email Address"
-                        className="w-full border-2 border-gray-400 rounded-md  lg:p-2"
+                        className="w-full  text-white font-extrabold  border-2 border-gray-400 rounded-md  lg:p-2 bg-inherit outline-none"
                       ></input>
     
                       <input
@@ -54,36 +52,27 @@ function Login(){
                         name="password"
                         required
                         placeholder="Password"
-                        className="w-full border-2 border-gray-400 rounded-md   lg:p-2"
+                        className="w-full  text-white font-extrabold  border-2 border-gray-400 rounded-md lg:p-2 bg-inherit outline-none"
                       ></input>
     
                       <div className="flex relative left-0 w-full mt-7">
                         <input type="checkbox" name="check" required></input>
-                        <label className="ml-2">
+                        <label className="ml-2 text-white font-extrabold">
                           Accept the Terms & Conditions
                         </label>
                       </div>
     
-                      <button className="bg-black text-white w-full rounded-lg p-2 ">
+                      <button className="bg-white w-full rounded-lg p-2 font-extrabold">
                         Join us →
                       </button>
                       <p className="mt-5 text-sm">or</p>
-                      <div className="w-full border-t-2 border-b-slate-800 p-2">
-                        <button className="bg-white text-black w-full rounded-lg p-3 mt-4 border-2 shadow-md flex items-center justify-center text-sm">
-                          <FaGoogle className="text-blue-500 text-xs mr-2" /> Sign
-                          up with Google
-                        </button>
-                        <button className="bg-black text-white w-full rounded-lg p-3 mt-4 border-2 shadow-md flex items-center justify-center text-sm">
-                          <FaApple className="text-sm mr-2" /> Sign up with Apple
-                        </button>
-                      </div>
+                   
                     </div>
                   </form>
                 </div>
     
-                <div className=" w-[50%] rounded-3xl overflow-hidden relative p-0 m-0 flex justify-center">
-                  <img  src="./src/assets/logo.JPG" className=" w-full h-full object-fill "
-                  ></img>
+                <div className=" w-[50%] rounded-3xl overflow-hidden relative p-0 m-0 flex justify-center bg-[#121212b0]">
+                  
                   <h1 className="text-white z-10  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-60 text-center text-5xl ">
                     Already have an account?
                   </h1>
@@ -94,9 +83,8 @@ function Login(){
               </>
             ) : (
               <>
-              <div className=" w-[50%] rounded-3xl overflow-hidden relative p-0 m-0 flex justify-center">
-                  <img src="./src/assets/logo.JPG" className=" w-full h-full object-fill "
-                  ></img>
+                <div className=" w-[50%] rounded-3xl overflow-hidden relative p-0 m-0 flex justify-center bg-[#121212b0]">
+                  
                   <h1 className="text-white z-10  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-60 text-center text-5xl ">
                     Create your Account
                   </h1>
@@ -107,14 +95,14 @@ function Login(){
                 <div className="w-[50%] h-[100%] border-none">
                   <form className="flex flex-col justify-center items-center w-full">
                     <h1 className="text-4xl font-bold text-black mt-14">Sign in</h1>
-                    <div className="flex flex-col justify-center items-center w-full  p-10 mt-5">
+                    <div className="flex flex-col justify-center items-center w-full  p-10 mt-10">
     
                       <input
                         type="text"
                         name="email"
                         required
-                        placeholder="Email Address"
-                        className="w-full border-2 border-gray-400 rounded-md p-2 mt-5"
+                        placeholder="Email Address" 
+                        className="w-full border-2 border-gray-400 rounded-md p-2 mt-5 bg-inherit text-white font-extrabold outline-none"
                       ></input>
     
                       <input
@@ -122,23 +110,12 @@ function Login(){
                         name="password"
                         required
                         placeholder="Password"
-                        className="w-full border-2 border-gray-400 rounded-md p-2 mt-5"
+                        className="w-full border-2 border-gray-400 rounded-md p-2 mt-5  bg-inherit text-white font-extrabold outline-none"
                       ></input>
-    
-    
-                      <button className="bg-black text-white w-full rounded-lg p-2 mt-5 ">
+
+                      <button className="bg-white text-black w-full rounded-lg p-2 mt-8 font-extrabold">
                         Sign in →
                       </button>
-                      <p className="mt-5 text-sm">or</p>
-                      <div className="w-full border-t-2 border-b-slate-800 p-2">
-                        <button className="bg-white text-black w-full rounded-lg p-3 mt-4 border-2 shadow-md flex items-center justify-center text-sm">
-                          <FaGoogle className="text-blue-500 text-xs mr-2" /> Sign
-                          in with Google
-                        </button>
-                        <button className="bg-black text-white w-full rounded-lg p-3 mt-4 border-2 shadow-md flex items-center justify-center text-sm">
-                          <FaApple className="text-sm mr-2" /> Sign in with Apple
-                        </button>
-                      </div>
                     </div>
                     <a href='https://en.wikipedia.org/wiki/SpongeBob_SquarePants'>
                       <img src="./src/assets/spongeBob.gif" className="w-[300px]"></img>
